34131d2858920515f5abc4b3b5e6ee55379c83c3
Pierre-Chesne <pierrc@hotmail.com>
2022-10-03 19:05:43 +0200

    test workflows issue fmt

--- Modules/Az_Container_Apps/version.tf
+++ Modules/Az_Container_Apps/version.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version ="= 3.23.0"
+      version = "= 3.23.0"
     }
 
     azapi = {
-      source ="Azure/azapi"
+      source = "Azure/azapi"
     }
   }
 }
