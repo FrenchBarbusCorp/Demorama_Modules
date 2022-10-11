7aa255d09762c850ee27078a3c6165306155f5c0
CHESNE Pierre <pierrc@hotmail.com>
2022-10-11 18:21:54 +0200

    Create variables.tf

---
+++ Modules/Az_Container_Apps/variables.tf
@@ -0,0 +1,31 @@
+variable "rgName" {
+  type = string
+}
+
+variable "azRegion" {
+  type = string
+}
+
+variable "acrName" {
+  type = string
+}
+
+variable "acrSku" {
+  type = string  
+}
+
+variable "logAnalyticsWorkSpaceName" {
+  type = string
+}
+
+variable "logAnalyticsWorkSpaceSku" {
+  type = string
+}
+
+variable "logAnalyticsWorkSpaceRetentionDays" {
+  type = number
+}
+
+variable "envName" {
+  type = string
+}
